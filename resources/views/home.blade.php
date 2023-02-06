9962536173702304800790c11c0bf34c672d23b7
Jaylord F <simplyniceweb@gmail.com>
2023-02-06 20:06:09 +0800

    text

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,14 +2,14 @@
     <div class="flex justify-center mt-10 relative z-20">
         <img src="images/front.png" alt="">
         <div class="ro-name absolute bottom-5 center drop-shadow-lg">
-            <h2 class="text-4xl text-white font-bold uppercase drop-shadow-lg">Ragnarok Old World</h2>
+            <h2 class="text-5xl text-white font-bold uppercase drop-shadow-lg">Ragnarok Old World</h2>
         </div>
     </div>
 
     <div class="flex justify-center relative z-10 -mt-7">
         <div class="bg-white p-3 rounded w-full">
             <div class="grid grid-cols-3 gap-4">
-                <div><img src="images/classes.jpg" class="rounded-tl rounded-bl" alt="classes"></div>
+                <div><img src="images/classes.jpg" class="rounded" alt="classes"></div>
                 <div class="grid grid-rows-3 drop-shadow-md">
                     <a href="" class="w-full font-semibold drop-shadow-md bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-r hover:from-blue-500 hover:to-cyan-500 rounded p-2 my-3 text-white">DOWNLOAD FULL CLIENT</a>
                     <a href="" class="w-full font-semibold drop-shadow-md bg-gradient-to-r from-pink-500 to-red-500 hover:bg-gradient-to-r hover:from-red-500 hover:to-pink-500 rounded p-2 my-3 text-white">DOWNLOAD KRO CLIENT</a>
