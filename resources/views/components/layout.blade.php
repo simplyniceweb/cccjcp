d763422cd48275231ff714c57a60b159c8741162
Jaylord F <simplyniceweb@gmail.com>
2023-02-06 18:37:24 +0800

    links

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -16,7 +16,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">    
 </head>
-<body>
+<body class="bg-scroll bg-no-repeat bg-cover bg-right-bottom bg-emerald-700 pb-52" style="background-image: url('images/bg.png')">
     @include('_header')
 
     <div class="max-w-screen-xl m-auto">
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,2 +1,22 @@
 <x-layout>
+    <div class="flex justify-center mt-10 relative z-20">
+        <img src="images/front.png" alt="">
+        <div class="ro-name absolute bottom-5 center drop-shadow-lg">
+            <h2 class="text-3xl text-white font-bold uppercase drop-shadow-md">Ragnarok Old World</h2>
+        </div>
+    </div>
+
+    <div class="flex justify-center relative z-10 -mt-7">
+        <div class="bg-white p-3 rounded w-full">
+            <div class="grid grid-cols-3 gap-4">
+                <div><img src="images/classes.jpg" class="rounded-tl rounded-bl" alt="classes"></div>
+                <div class="grid grid-rows-3">
+                    <a href="" class="w-full bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-r hover:from-blue-500 hover:to-cyan-500 rounded p-2 my-3 text-white">DOWNLOAD FULL CLIENT</a>
+                    <a href="" class="w-full bg-gradient-to-r from-pink-500 to-red-500 hover:bg-gradient-to-r hover:from-red-500 hover:to-pink-500 rounded p-2 my-3 text-white">DOWNLOAD KRO CLIENT</a>
+                    <a href="" class="w-full bg-gradient-to-r from-lime-500 to-green-500 hover:bg-gradient-to-r hover:from-green-500 hover:to-lime-500 rounded p-2 my-3 text-white">DOWNLOAD SMALL CLIENT</a>
+                </div>
+                <div><img src="images/classes.jpg" class="rounded-tl rounded-bl" alt="classes"></div>
+            </div>
+        </div>
+    </div>
 </x-layout>
