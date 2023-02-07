aa977e3105237749d2d3ff9e30b9ae08ea0cfb96
Jaylord F <simplyniceweb@gmail.com>
2023-02-07 21:28:14 +0800

    update

--- app/Http/Controllers/CharacterController.php
+++ app/Http/Controllers/CharacterController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -9,12 +10,12 @@ class CharacterController extends Controller
 {
     public function resetPosition(?int $char_id)
     {
-        $character = DB::table('char')->where('char_id', $char_id)->first();
-
-        $character->last_map = 'prontera';
-        $character->last_x = '155';
-        $character->last_y = '180';
-        $character->save();
+        User::where('char_id', $char_id)
+        ->update([
+            'last_map' => 'prontera',
+            'last_x' => '155',
+            'last_y' => '180',
+        ]);
 
         return redirect('/');
     }
