484759a6bff18f44ffc0067995337153a0ad7561
Jaylord F <simplyniceweb@gmail.com>
2023-02-17 18:12:09 +0800

    signup mobile

--- resources/views/security/registration.blade.php
+++ resources/views/security/registration.blade.php
@@ -4,8 +4,8 @@
     id="authentication-modal" 
     tabindex="-1" 
     aria-hidden="true" 
-    class="fixed z-20 h-3/4 w-1/4 m-auto inset-x-0 inset-y-0" style="display: none">
-    <div class="relative w-full h-full max-w-md md:h-auto">
+    class="fixed z-20 h-3/4 w-full md:w-1/4 m-auto inset-x-0 inset-y-0" style="display: none">
+    <div class="relative w-full h-full md:max-w-md md:h-auto">
         <!-- Modal content -->
         <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
             <button x-on:click="open = ! open" type="button" class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white" data-modal-hide="authentication-modal">
